e87f6a985139810050108e2404021973dd9a30b0
abein <kazu.abe1945@gmail.com>
2023-06-13 04:00:25 +0900

    キャッシュ使わないようにしてみる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { SampleCard, FadeIn } from './../components/sample_cell';
 import { SuccessResult } from './../service/api';
 
 const page = async () => {
-  //const reqPostsResult: Alcohols = ((await reqAlcohols.get(1)) as SuccessResult<Alcohols>).response.data
-  const Result = await fetch(`https://project-sake.an.r.appspot.com:443/categories/1/alcohols`);
-  const reqPostsResult: Alcohols = (await (Result.json() as  Promise<Alcohols>));
+  const reqPostsResult: Alcohols = ((await reqAlcohols.get(1)) as SuccessResult<Alcohols>).response.data
+  //const Result = await fetch(`https://project-sake.an.r.appspot.com:443/categories/1/alcohols`);
+  //const reqPostsResult: Alcohols = (await (Result.json() as  Promise<Alcohols>));
 
 
   return (
