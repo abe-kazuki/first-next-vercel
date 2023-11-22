cc8590cd431d9c01d7c80cfdbf9bdf57109c19f9
abein <kazu.abe1945@gmail.com>
2023-11-23 02:55:27 +0900

    コメントpost apiコールまで

--- src/components/details/comment.tsx
+++ src/components/details/comment.tsx
@@ -3,7 +3,7 @@ import {FC, useState, useEffect} from 'react';
 import {CommentForm} from './../comments/form'
 import {CommentContent} from './../comments/content'
 import styled from 'styled-components';
-import {Comments, reqCommentsGet} from '../../lib/meigara/commentJsonPlaceholder';
+import {Comments, reqComments, PostCommentRes} from '../../lib/meigara/commentJsonPlaceholder';
 import {FailResult, SuccessResult} from "~/src/service/api";
 
 const CommentBase = styled.div`
@@ -24,12 +24,23 @@ export const Comment: FC<Props> = (prop: Props) => {
         console.log(`文字列追加されているせ${inputComment}`)
       };
     const handleSubmit = (e: any) => {
-        console.log("サブミットされたぜ！！")
-        // API通信
+        e.preventDefault();
+        const reqPostsResult = reqComments.post(prop.meigara_id, inputComment)
+        reqPostsResult?.then(
+            (res: SuccessResult<PostCommentRes> | FailResult<PostCommentRes>) => {
+                const result: PostCommentRes = (res as SuccessResult<PostCommentRes> ).response.data
+                
+                // 新しいコメントを既存のコメントに追加してステートを更新
+                setComments((prevComments) => ({
+                    comments: [...prevComments.comments, result.comment],
+                }));
+            }).catch((res: FailResult<PostCommentRes>) =>{
+                alert(`コメントの追加に失敗しました。`);
+              })
         // 結果をコメントにappendする
     };
     useEffect(() => {
-        const reqPostsResult = reqCommentsGet.get(prop.meigara_id)
+        const reqPostsResult = reqComments.get(prop.meigara_id)
         reqPostsResult?.then(
             (res: SuccessResult<Comments> | FailResult<Comments>) => {
                 const result: Comments = (res as SuccessResult<Comments> ).response.data
